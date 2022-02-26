4ec911c9b7c954af8c7fc314228fedafc25076f3
TaeWoo <43276268+ing9990@users.noreply.github.com>
2022-02-26 12:54:34 +0900

    MemberService
    @RequiredArgsConstructor

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.item.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -11,30 +12,29 @@ import java.util.List;
  * @author TaeWK
  */
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
-    
-    @PersistenceContext
-    private EntityManager em;
 
-    public void save(Member member){
+    private final EntityManager em;
+
+    public void save(Member member) {
         em.persist(member);
     }
 
-    public Member findOne(Long id){
-        return em.find(Member.class,id);
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
     }
 
-    public List<Member> findAll(){
+    public List<Member> findAll() {
         return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
     }
 
-    public List<Member> findByName(String name){
-        return (List<Member>) em.createQuery("select m from Member m where m.name=:name",Member.class)
-                .setParameter("name",name)
+    public List<Member> findByName(String name) {
+        return (List<Member>) em.createQuery("select m from Member m where m.name=:name", Member.class)
+                .setParameter("name", name)
                 .getResultList();
     }
-
 
 
 }
